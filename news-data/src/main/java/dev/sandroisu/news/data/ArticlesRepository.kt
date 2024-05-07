cf268ee91b22ce02000e3791b5aba0e20b9ed709
Sandroisu <sandroisu1411@gmail.com>
2024-05-07 21:19:45 +0300

    доработал getAll4

--- news-data/src/main/java/dev/sandroisu/news/data/ArticlesRepository.kt
+++ news-data/src/main/java/dev/sandroisu/news/data/ArticlesRepository.kt
@@ -1,39 +1,42 @@
 package dev.sandroisu.news.data
 
 import dev.sandroisu.news.data.model.Article
-import dev.sandroisu.news.data.model.toArticle
+import dev.sandroisu.news.data.model.toArticleDbo
 import dev.sandroisu.news.database.NewsDatabase
+import dev.sandroisu.news.database.models.ArticleDBO
 import dev.sandroisu.newsapi.NewsApi
+import dev.sandroisu.newsapi.models.ArticleDTO
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
-import java.io.IOException
+import kotlinx.coroutines.flow.onEach
 
 class ArticlesRepository(
     private val database: NewsDatabase,
     private val api: NewsApi,
 ) {
     fun getAll(): Flow<RequestResult<List<Article>>> {
-        val cachedAllArticles =
-            database.articlesDao
-                .getAll()
-                .map { articles ->
-                    articles.map { it.toArticle() }
-                }
+        val cachedAllArticles: Flow<RequestResult.Success<List<ArticleDBO>>> = getAllFromDatabase()
         val remoteArticles =
             flow {
                 emit(api.everything())
             }.map { result ->
                 if (result.isSuccess) {
                     val response = result.getOrThrow()
-                    response.articles
+                    RequestResult.Success(response.articles)
                 } else {
-                    throw result.exceptionOrNull() ?: IOException("Unknown error")
+                    RequestResult.Error(null)
                 }
-            }.map { articlesDtos ->
-                articlesDtos.map { articleDto -> articleDto.toArticleDbo() }
-            }
+            }.filterIsInstance<RequestResult.Success<List<ArticleDTO>?>>()
+                .map { successResult ->
+                    successResult.requreData()
+                        .map { articleDTO -> articleDTO.toArticleDbo() }
+                        .let { RequestResult.Success<List<ArticleDBO>?>(it) }
+                }.onEach { requestResult ->
+                    database.articlesDao.insert(requestResult.requreData())
+                }
 
         cachedAllArticles.combine(remoteArticles)
         return flow {
@@ -43,15 +46,48 @@ class ArticlesRepository(
         }
     }
 
+    private fun getAllFromDatabase(): Flow<RequestResult.Success<List<ArticleDBO>>> {
+        return database.articlesDao
+            .getAll()
+            .map { RequestResult.Success(it) }
+    }
+
+    private fun getAllFromServer(): Flow<RequestResult.Success<List<ArticleDBO>?>> {
+        return flow {
+            emit(api.everything())
+        }.map { result ->
+            if (result.isSuccess) {
+                val response = result.getOrThrow()
+                RequestResult.Success(response.articles)
+            } else {
+                RequestResult.Error(null)
+            }
+        }.filterIsInstance<RequestResult.Success<List<ArticleDTO>?>>()
+            .map { successResult ->
+                successResult.requreData()
+                    .map { articleDTO -> articleDTO.toArticleDbo() }
+                    .let { RequestResult.Success<List<ArticleDBO>?>(it) }
+            }.onEach { requestResult ->
+                database.articlesDao.insert(requestResult.requreData())
+            }
+    }
+
     suspend fun search(query: String): Flow<Article> {
         api.everything()
     }
 }
 
-sealed class RequestResult<E>(protected val data: E?) {
+sealed class RequestResult<E>(internal val data: E?) {
     class InProgress<E>(data: E?) : RequestResult<E>(data)
 
     class Success<E>(data: E?) : RequestResult<E>(data)
 
     class Error<E>(data: E?) : RequestResult<E>(data)
+}
+
+fun <T : Any> RequestResult<T?>.requreData(): T {
+    return checkNotNull(data)
+}
+
+internal fun <I, O> RequestResult<I>.map(mapper: (I) -> O): RequestResult<O> {
 }
--- news-data/src/main/java/dev/sandroisu/news/data/model/Mappers.kt
+++ news-data/src/main/java/dev/sandroisu/news/data/model/Mappers.kt
@@ -1,6 +1,12 @@
 package dev.sandroisu.news.data.model
 
 import dev.sandroisu.news.database.models.ArticleDBO
+import dev.sandroisu.newsapi.models.ArticleDTO
 
 internal fun ArticleDBO.toArticle(): Article {
+    TODO("Not yet implemented")
+}
+
+internal fun ArticleDTO.toArticleDbo(): ArticleDBO {
+    TODO("Not yet implemented")
 }
