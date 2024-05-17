8cff76ce5d6c871b5550cb394ba5e227825d4989
Sandroisu <sandroisu1411@gmail.com>
2024-05-17 21:47:18 +0300

    поправил возвращемый тип для метода

--- features/news-main/src/main/java/dev/sandroisu/news/main/GetAllArticlesUseCase.kt
+++ features/news-main/src/main/java/dev/sandroisu/news/main/GetAllArticlesUseCase.kt
@@ -1,11 +1,12 @@
 package dev.sandroisu.news.main
 
 import dev.sandroisu.news.data.ArticlesRepository
+import dev.sandroisu.news.data.RequestResult
 import dev.sandroisu.news.data.model.Article
 import kotlinx.coroutines.flow.Flow
 
 class GetAllArticlesUseCase(private val repository: ArticlesRepository) {
-    operator fun invoke(): Flow<List<Article>> {
+    operator fun invoke(): Flow<RequestResult<List<Article>>> {
         return repository.getAll()
     }
 }
