29870d3090188eb0409ed9c9e4e2e674537eb47f
Sandroisu <sandroisu1411@gmail.com>
2024-05-23 23:55:13 +0300

    небольшие правки

--- news-data/src/main/java/dev/sandroisu/news/data/model/Mappers.kt
+++ news-data/src/main/java/dev/sandroisu/news/data/model/Mappers.kt
@@ -4,11 +4,9 @@ import dev.sandroisu.news.database.models.ArticleDBO
 import dev.sandroisu.newsapi.models.ArticleDTO
 
 internal fun ArticleDBO.toArticle(): Article {
-    TODO("Not yet implemented")
+    TODO("Need to implement")
 }
 
 internal fun ArticleDTO.toArticleDbo(): ArticleDBO {
-    return ArticleDBO(
-        author = author,
-    )
+    TODO("Need to implement")
 }
