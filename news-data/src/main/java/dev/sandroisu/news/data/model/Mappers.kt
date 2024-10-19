9d53722bd1ff8fb48a7d38af22edfc1ec6bfc4d2
Александр Слатинин <alex@MacBook-Pro-Aleksandr.local>
2024-10-19 21:04:50 +0300

    добавил заглушку для маппера

--- news-data/src/main/java/dev/sandroisu/news/data/model/Mappers.kt
+++ news-data/src/main/java/dev/sandroisu/news/data/model/Mappers.kt
@@ -2,11 +2,12 @@ package dev.sandroisu.news.data.model
 
 import dev.sandroisu.news.database.models.ArticleDBO
 import dev.sandroisu.newsapi.models.ArticleDTO
+import java.util.Date
 
 internal fun ArticleDBO.toArticle(): Article {
-    TODO("Need to implement")
+    return Article(0, Source("", ""), "", "", "", "", "", Date(), "")
 }
 
 internal fun ArticleDTO.toArticleDbo(): ArticleDBO {
-    TODO("Need to implement")
+
 }
