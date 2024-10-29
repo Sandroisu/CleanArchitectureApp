e51a5f79b0a2964b9a06495b0c8bef4f4f052f59
Alex Slatinin <sandroisu1411@gmail.com>
2024-10-29 06:46:27 +0300

    small refactoring

--- news-data/src/main/java/dev/sandroisu/news/data/ArticlesRepository.kt
+++ news-data/src/main/java/dev/sandroisu/news/data/ArticlesRepository.kt
@@ -76,18 +76,16 @@ class ArticlesRepository(
 sealed class RequestResult<out E : Any>(val data: E? = null) {
     class InProgress<E : Any>(data: E? = null) : RequestResult<E>(data)
 
-    class Success<E : Any>(data: E) : RequestResult<E>(data)
+    class Success<E : Any>(data: E) : RequestResult<E>(data) {
+        fun requireData(): E = checkNotNull(data)
+    }
 
     class Error<E : Any>(data: E? = null, val error: Throwable? = null) : RequestResult<E>(data)
 }
 
 fun <I : Any, O : Any> RequestResult<I>.map(mapper: (I) -> O): RequestResult<O> {
     return when (this) {
-        is RequestResult.Success -> {
-            val outData: O = mapper(checkNotNull(data))
-            RequestResult.Success(checkNotNull(outData))
-        }
-
+        is RequestResult.Success -> RequestResult.Success(mapper(requireData()))
         is RequestResult.Error -> RequestResult.Error()
         is RequestResult.InProgress -> RequestResult.InProgress(data?.let(mapper))
     }
