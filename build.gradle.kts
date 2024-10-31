be6dc45d9078fb1f3476220e0ba9a3a96663d899
Alex Slatinin <sandroisu1411@gmail.com>
2024-10-31 07:22:20 +0300

    added hilt dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinJvm) apply false
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.dagger.hilt.android) apply false
 }
